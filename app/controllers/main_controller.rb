c1ee0577b82ccf6ad32e31634a048a26de2c14a2
Thien Nguyen <41944095+s3714217@users.noreply.github.com>
2021-05-07 23:34:02 +1000

    Add files via upload

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,7 +1,13 @@
 class MainController < ApplicationController
   def main
-    @current_user="Guest"
+    @current_user= User.first
     @items = Item.all
+    @selected_item = params[:selected_item]
+    if @selected_item != nil
+      ActiveRecord::Base.connection.execute("INSERT INTO 'items_users' (item_id, user_id) VALUES (#{params[:selected_item]},#{@current_user.id})")
+      puts "ADDED #{params[:selected_item]} to #{@current_user.id}"
+      @selected_item = nil
+    end
   end
 
   def about
