3c2ead0238e6a1da04cf7f2fda8ef94b0687cda7
Thien Nguyen <41944095+s3714217@users.noreply.github.com>
2021-05-10 13:13:33 +1000

    Add files via upload

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
-  root 'application#hello'
+  get 'login/login' => 'login#login'
+  get 'login/sign_up' => 'login#sign_up'
+  post 'login/sign_up' => 'login#sign_up'
+
+  resources :items
+  resources :users do
+    member do
+      post :remove_items, to: 'users#remove_items'
+    end
+  end
+  resources :subscribers
+  get 'main/main' => 'main#main'
+  post 'main/main' => 'main#main'
+
+  get 'main/about'
+
+  get 'main/contact'
+  
+  get 'main/checkout'
+  
+  get 'newsletter_signup', to: 'subscribers#new'
+  post 'newsletter_signup', to: 'subscribers#create'
+
+  root 'main#main'
 end
