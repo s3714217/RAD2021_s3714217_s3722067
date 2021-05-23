e7235a1fb620ac76534ad6935cf2f302bcd64e08
DannyLeCode <danny_le@y7mail.com>
2021-05-23 08:41:17 +0000

    Revert "Merge pull request #13 from s3714217/submission"
    This reverts commit 17e24fff1c83fd1ab9db1b4dabc781dcf6486e31, reversing
    changes made to 3c2ead0238e6a1da04cf7f2fda8ef94b0687cda7.

--- db/migrate/20210508105317_add_columns_to_items.rb
+++
@@ -1,8 +0,0 @@
-class AddColumnsToItems < ActiveRecord::Migration[5.1]
-  def change
-    add_column :items, :tag, :string
-    add_column :items, :colour, :string
-    add_column :items, :size, :string
-    add_column :items, :popularity, :integer
-  end
-end
